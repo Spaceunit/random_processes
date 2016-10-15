139036b498c1097721df60fbcc7fa908ead6d23a
Oleksiy <oleksiypolshchak@gmail.com>
2016-10-15 22:18:24 +0300

    delete new elements

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ import udl
 
 class Work:
     def __init__(self):
-        #
-        app = QApplication(sys.argv)
-        w = QWidget()
-        w.resize(250, 150)
-        w.move(300, 300)
-        w.setWindowTitle('Simple')
-        w.show()
-        #
-        sys.exit(app.exec_())
         self.a = matrix.Matrix([[0]],"Initial matrix")
         self.accuracy = 3
         self.commands = {
